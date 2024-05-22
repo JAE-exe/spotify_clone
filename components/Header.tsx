b9bffd6e69bf7a18510e05e7641d1e4f3e0f7a78
Jayden De Souza <jaydendesouza2005@gmail.com>
2024-05-22 12:55:58 +0530

    commit 7

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,10 @@
 "use client";
-import { useRouter } from "next/navigation"; // Correct import for useRouter from next/navigation
+import { useRouter } from "next/navigation";
+import { BiSearchAlt } from "react-icons/bi";
 import { HiHome } from "react-icons/hi";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { twMerge } from "tailwind-merge";
+import Button from "./Button";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -19,7 +21,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
         className
       )}
     >
-      <div className="w-full mb-4 flex items-center">
+      <div className="w-full mb-4 flex items-center justify-between">
         <div className="hidden md:flex gap-x-2 items-center">
           <button
             onClick={() => router.back()}
@@ -34,10 +36,19 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             <RxCaretRight size={35} className="text-white" />
           </button>
         </div>
-        <div className="flex md:hidden gap-x-2 items-center ">
-          <button className="">
-            <HiHome size={20} className="text-white" />
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <HiHome size={20} className="text-black" />
           </button>
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <BiSearchAlt size={20} className="text-black" />
+          </button>
+        </div>
+        <div className="ml-auto flex items-center gap-x-4">
+          <Button className="bg-transparent text-neutral-300 font-medium">
+            Sign Up
+          </Button>
+          <Button className="bg-white px-6 py-2 ">Login</Button>
         </div>
       </div>
     </div>
