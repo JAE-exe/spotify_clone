10470a761317a70870ca0937a4b258883b13ef3e
Jayden De Souza <jaydendesouza2005@gmail.com>
2024-05-24 22:00:16 +0530

    commit 11.2

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <Header>
         <div className="mb-2">
           <h1 className="text-white font-semibold text-3xl">Welcome back!</h1>
-          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mb-4 ">
+          <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mb-4">
             <ListItem
               image="/images/liked.jpg"
               name="Liked Songs"
