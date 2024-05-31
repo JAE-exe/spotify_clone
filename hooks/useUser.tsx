c721d87193f7470b2b6d0a176c1c9fe34b4fddec
Jayden De Souza <jaydendesouza2005@gmail.com>
2024-05-31 21:59:16 +0530

    commit 15

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -1,10 +1,34 @@
+import { Subscription, UserDetails } from "@/types";
+import { User } from "@supabase/auth-helpers-nextjs";
+import {
+  useSessionContext,
+  useUser as useSupaUser,
+} from "@supabase/auth-helpers-react";
+import { createContext, ReactNode } from "react";
 
-const useUser = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+type UserContextType = {
+  access_token: string | null;
+  user: User | null;
+  userDetails: UserDetails | null;
+  isLoading: boolean;
+  subscription: Subscription | null;
 };
 
-export default useUser
+export const UserContext = createContext<UserContextType | undefined>(
+  undefined
+);
+
+export interface Props {
+  children: ReactNode;
+}
+
+export const MyUserContextProvider = ({ children }: Props) => {
+  const {
+    session,
+    isLoading: isLoadingUser,
+    supabaseClient: supabase,
+  } = useSessionContext();
+
+  const user = useSupaUser();
+  const accessToken = session?.access_token ?? null;
+};
